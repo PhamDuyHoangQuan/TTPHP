55d762e57a3f6ee49252e9d74777c02cb5bff0de
PhamDuyHoangQuan <101261380+PhamDuyHoangQuan@users.noreply.github.com>
2022-03-11 09:40:00 +0700

    Add files via upload

--- Ex1.php
+++ Ex1.php
@@ -15,15 +15,18 @@
     function checkfile(){
         $file1 = 'English is a West Germanic language that was first spoken in early medieval England and is now the third most widespread native language in the world, after Standard Chinese and Spanish, as well as the most widely spoken Germanic language. Named after the Angles, one of the Germanic tribes that migrated to England, it ultimately derives its name from the Anglia (Angeln) peninsula in the Baltic Sea';
         $file2 = 'Modern English grammar is the result of a gradual change from a typical Indo-European dependent marking pattern with a rich inflectional morphology and relatively free word order';
-        if ($this -> checkvalidString($file1) == true){
+        $check1 = checkvalidString($file1);
+        $check2 = checkvalidString($file2);
+        if ($check1 == true){
             echo 'Chuỗi hợp lệ';
         }else{
             echo 'Chuỗi không hợp lệ';
         }
-        if ($this -> checkvalidString($file2) == true){
+        if ($check2 == true){
             echo 'Chuỗi hợp lệ';
         }else{
             echo 'Chuỗi không hợp lệ';
         }
     } 
+    checkfile();
 ?>
