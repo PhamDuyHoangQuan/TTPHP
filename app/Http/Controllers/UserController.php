7766a2194a64feb75a03bb81e4772e0dd910f1af
PhamDuyHoangQuan <101261380+PhamDuyHoangQuan@users.noreply.github.com>
2022-03-30 09:20:19 +0700

    Laravel immediate 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,41 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
-class UserController extends Controller
+use App\Models\User;
+use App\Http\Requests\CreateUserRequest;
+class UsersController extends Controller
 {
-    //
+        protected $user;
+    
+    public function __construct(User $user){
+        $this->user = $user;
+    }
+    function index(){
+                if(request()->key){
+           dd(request()->key);
+        }
+    }
+    function getregister(){
+     return view('Register',['title'=>'thêm người dùng']);
+    }
+
+    function postregister(CreateUserRequest $request){
+    function postregister(CreateUserRequest $request)
+    {
+
+      $user->checkregister($request);
+      return redirect('list');
+      $user = $user->checkregister($request);
+
+      if($user)
+      {
+        Session::flash('success','tạo thành công tài khoản');
+        return redirect('list');
+      }
+    }
+    function view(){
+
+        return view('list',['listall'=> $user->listAll(),'title'=>'danh sach']);
+    }
+}
 }
---
+++ views/layouts/header.blade.php
@@ -0,0 +1,19 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel='stylesheet' type='text/css' href='./css/style.css'/>
+
+        <title>Admin</title>
+    </head>
+  <body>
+      <header>
+          <h2>Admin</h2>
+      </header>
+            <div id="menu">
+                <ul>
+                    <li><a href="#">Thêm thành viên mới</a></li>
+                    <li><a href="#">Danh sách thành viên</a></li>
+                </ul>
+            </div>
+    
